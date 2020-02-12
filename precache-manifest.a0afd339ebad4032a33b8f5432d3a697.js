78f79196850db6f3b4eb4c4d0c0bcc887b257fe8
Vadzim <vadzimfiadoryn@gmail.com>
2020-02-12 16:54:25 +0300

    Updates

---
+++ precache-manifest.a0afd339ebad4032a33b8f5432d3a697.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e2327d8741f016c975332a12baa8d70",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "f0a626b2d86c5d1ea60c",
+    "url": "/portfolio/static/css/main.cc60aa14.chunk.css"
+  },
+  {
+    "revision": "372e2f4223a50f94d597",
+    "url": "/portfolio/static/js/2.d4d03422.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/portfolio/static/js/2.d4d03422.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0a626b2d86c5d1ea60c",
+    "url": "/portfolio/static/js/main.945cb2df.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  }
+]);
